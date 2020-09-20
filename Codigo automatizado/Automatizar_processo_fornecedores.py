7146dffde1249bec975cabe293d50b4f727f385a
PedroAccarini <71603878+PedroAccarini@users.noreply.github.com>
2020-09-20 17:19:36 -0300

    Add files via upload

---
+++ Codigo automatizado/Automatizar_processo_fornecedores.py
@@ -0,0 +1,14 @@
+import glob
+import csv
+
+with open("planilha_final.csv", 'w') as final:
+    w = csv.writer(final)
+
+    for nf in glob.glob("Fornecedores/*.csv", recursive=True):
+
+        with open(nf) as csv_file:
+            temp = csv.reader(csv_file)
+
+            for row in temp:
+                w.writerow(row + [';'] +
+                           [str(nf).split('\\')[-1].split('.')[-2]])
